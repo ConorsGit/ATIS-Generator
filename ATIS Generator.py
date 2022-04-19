4bce077a0cddfa4693a0734b93c6d9773bcae38a
ConorsGit <85834862+ConorsGit@users.noreply.github.com>
2022-04-19 10:43:31 +0100

    Major Upgrade
    Supports multiple runways
    Beeps when your ATIS gets updated so you don't miss it (windows only)
    More efficient code

--- ATIS Generator.py
+++ ATIS Generator.py
@@ -1,7 +1,7 @@
-from cgitb import text
-import time
 import os
-import urllib
+import time
+import winsound
+import requests
 
 ###################################################
 key = "PUT YOUR KEY HERE"
@@ -10,7 +10,7 @@ key = "PUT YOUR KEY HERE"
 voiceUrl = "http://api.voicerss.org/?key=" + key + "&hl=en-gb&v=Harry&c=MP3&f=8khz_8bit_mono&src="
 
 voiceTemplate = """{airport} INFORMATION {letter}, TIME {validTime} zulu.
-DEPARTURE RUNWAY {depRunway}, ARRIVAL RUNWAY {arrRunway}.
+DEPARTURE RUNWAY {departureRunway}, ARRIVAL RUNWAY {arrivalRunway}.
 TRANSITION LEVEL FLIGHT LEVEL 1 5, SURFACE WIND VARIABLE.
 VISIBILITY 10 KILOMETRES OR MORE.
 TEMPERATURE PLUS 6, DEW POINT PLUS 5.
@@ -24,8 +24,8 @@ Valid as of {validTime}z.
 
 Airspace Ceiling: FL060
 
-Departure Runway: {depRunway}
-Arrival Runway: {arrRunway}
+Departure Runway: {departureRunway}
+Arrival Runway: {arrivalRunway}
 Max Taxi Speed: 25kts
 
 Advise aircraft type on first contact and advise you have information {letter} onboard.
@@ -34,101 +34,100 @@ NOTAMS:
 - Realistic emergencies are authorised.
 - Providing top-down control over airspace.
 - All aircraft must use latest charts (https://github.com/Treelon/ptfs-charts/tree/main/).
-- CPDLC is avaliable (you may request and recieve IFR clearance in game chat)."""
+- PDC is avaliable (you may request and recieve IFR clearance in game chat)."""
 
-#--airport name, letter, time, departure runway, arrival runway, letter
-
-phoneticNumbers = {"0":"zero", "1":"one", "2":"two", "3":"tree", "4":"four", "5":"fife", "6":"six", "7":"seven", "8":"eight", "9":"niner"}
+phoneticNumbers = ["zero", "one", "two", "tree", "four", "fife", "six", "seven", "eight", "niner"]
 phoneticAlphabet = ["alpha", "bravo", "charlie", "delta", "echo", "foxtrot", "golf", "hotel", "india", "juliet", "kilo", "lima", "mike", "november", "oscar", "papa", "quebec", "romeo", "sierra", "tango", "uniform", "victor", "whiskey", "x-ray", "yankee", "zulu"]
 
-specialPhonetics = {
-    "R": "right",
-    "L": "left",
-    "C": "centre",
-    " ": ""
-}
-
-print("ATIS Generator - Do not close this window if you want ATIS to be automatically generated every 30 minutes.")
-print("▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬")
-airport = input("Airport Name: ")
-departureRunway = input("Departure Runway: ").upper()
-arrivalRunway = input("Arrival Runway: ").upper()
 atisLetterCode = -1
-atisLetter = "A"
-textAtis = ""
-voiceAtis = ""
-
-if len(departureRunway) == 2:
-    departureRunway = departureRunway + " "
-
-if len(arrivalRunway) == 2:
-    arrivalRunway = arrivalRunway + " "
-
-voiceDepartureRunway = list(departureRunway)
-voiceArrivalRunway = list(arrivalRunway)
-
-print(voiceDepartureRunway[2])
-L0 = phoneticNumbers[voiceDepartureRunway[0]]
-L1 = phoneticNumbers[voiceDepartureRunway[1]]
-L2 = specialPhonetics[voiceDepartureRunway[2]]
-voiceDepartureRunway = L0 + " " + L1 + " " + L2
-
-L0 = phoneticNumbers[voiceArrivalRunway[0]]
-L1 = phoneticNumbers[voiceArrivalRunway[1]]
-L2 = specialPhonetics[voiceArrivalRunway[2]]
-voiceArrivalRunway = L0 + " " + L1 + " " + L2
 
 def updateAtisLetter():
     global atisLetterCode
-    global atisLetter
     atisLetterCode += 1
 
     if atisLetterCode > 25:
         atisLetterCode -= 26
       
-    atisLetter = chr(65 + atisLetterCode)
+    return chr(65 + atisLetterCode)
 
-def getTime():
+def fetchUTCTime():
     utcTime = time.gmtime()
-    strHour = str(utcTime.tm_hour)
-    strMin = str(utcTime.tm_min)
+    hourString = str(utcTime.tm_hour)
+    minuteString = str(utcTime.tm_min)
 
     if utcTime.tm_hour < 10:
-        strHour = "0" + strHour
+        hourString = "0" + hourString
 
     if utcTime.tm_min < 10:
-        strMin = "0" + strMin
+        minuteString = "0" + minuteString
 
-    return strHour + strMin
+    return hourString + minuteString
 
-def formatTextAtis():
-    global textAtis
-    atisTime = getTime()
+def phoneticRunway(runway):
+    runwaySplit = list(runway)
 
-    textAtis = textTemplate.format(airport = airport.title(), letter = atisLetter, validTime = atisTime, depRunway = departureRunway, arrRunway = arrivalRunway)
-    print(atisTime + "z: Information " + atisLetter) 
+    if len(runway) == 2:
+        runwaySplit.append("")
+    
+    runwaySplit[0] = phoneticNumbers[int(runwaySplit[0])]
+    runwaySplit[1] = phoneticNumbers[int(runwaySplit[1])]
 
-def formatVoiceAtis():
-    global voiceAtis
-    atisTime = list(getTime())
-    atisTime = phoneticNumbers[atisTime[0]] + " " + phoneticNumbers[atisTime[1]] + " " + phoneticNumbers[atisTime[2]] + " " + phoneticNumbers[atisTime[3]]
+    match runwaySplit[2]:
+        case "L":
+            runwaySplit[2] = "left"
+        case "R":
+            runwaySplit[2] = "right"
+        case "C":
+            runwaySplit[2] = "centre"
 
-    voiceAtis = voiceTemplate.format(airport = airport, letter = phoneticAlphabet[atisLetterCode], validTime = atisTime, depRunway = voiceDepartureRunway, arrRunway = voiceArrivalRunway)
+    return " ".join(runwaySplit)
 
+def runwaysToVoice(runwayList):
+    runwayString = phoneticRunway(runwayList[0])
+
+    if len(runwayList) > 1:
+        runwayList.pop(0)
+
+        for runway in runwayList:
+            runwayString = runwayString + " AND " + phoneticRunway(runway)
+
+    return runwayString
+
+
+
+print("ATIS Generator - Do not close this window if you want ATIS to be automatically generated every 30 minutes.")
+print("▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬")
+airport = input("Airport Name: ")
+departureRunwayInput = input("Departure Runway: ").upper().split("/")
+arrivalRunwayInput = input("Arrival Runway: ").upper().split("/")
 print("▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬")
 
+departureRunways = '/'.join(departureRunwayInput)
+phoneticDepartureRunways = runwaysToVoice(departureRunwayInput)
+
+arrivalRunways = '/'.join(arrivalRunwayInput)
+phoneticArrivalRunways = runwaysToVoice(arrivalRunwayInput)
+
 while True:
-    updateAtisLetter()
-    formatTextAtis()
-    formatVoiceAtis()
+    atisTime = fetchUTCTime()
+    atisLetter = updateAtisLetter()
+    phoneticAtisTime = phoneticNumbers[int(atisTime[0])] + " " + phoneticNumbers[int(atisTime[1])] + " " + phoneticNumbers[int(atisTime[2])] + " " + phoneticNumbers[int(atisTime[3])]
+    
+    textAtis = textTemplate.format(airport = airport.title(), letter = atisLetter, validTime = atisTime, departureRunway = departureRunways, arrivalRunway = arrivalRunways)
+    voiceAtis = voiceTemplate.format(airport = airport.title(), letter = phoneticAlphabet[atisLetterCode], validTime = phoneticAtisTime, departureRunway = phoneticDepartureRunways, arrivalRunway = phoneticArrivalRunways)
 
     file = open("ATIS.txt", "w", encoding="utf-8")
     file.write(textAtis)
     file.close()
 
-    url = voiceUrl + urllib.parse.quote(voiceAtis)
+    voiceAtisRequest = requests.get(voiceUrl + voiceAtis)
+    file = open('ATIS.mp3', 'wb')
+    file.write(voiceAtisRequest.content)
+    file.close()
 
-    os.system("curl --silent \"" + url + "\" -H \"Pragma: no-cache\" ..... -H \"Cache-Control: no-cached\" > ATIS.mp3 ")
-    os.system("start .")
-    os.system("ATIS.txt")
-    time.sleep(30 * 60)
+    winsound.Beep(750, 750)
+    print(atisTime + "z | Information " + atisLetter)
+
+    os.startfile("ATIS.txt")
+
+    time.sleep(1 * 60)
